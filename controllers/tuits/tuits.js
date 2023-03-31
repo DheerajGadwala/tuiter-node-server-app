a95452fe87b3cc32d494d4c31772155c28144698
Sai Dheeraj Reddy Gadwala <drgad24@gmail.com>
2023-03-31 18:59:02 -0400

    minor changes

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,24 +1,45 @@
 export default [
     {
-     "_id": "123", "topic": "Space", "username": "SpaceX",
-     "handle": "@spacex", "time": "2h", "image": "spacex.png",
-     "title": "SpaceX's Mission",
-     "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-     "liked": true, "likes": 2345, "replies": 123, "retuits": 432
+      "_id": "234",
+      "topic": "Space",
+      "userName": "SpaceX",
+      "time": "2h",
+      "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
+      "image": "/images/spacex.webp",
+      "liked": true,
+      "replies": 123,
+      "retuits": 432,
+      "likes": 2345,
+      "handle": "@spacex",
+      "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars"
     },
     {
-     "_id": "234", "topic": "Traffic", "username": "The Boring Company",
-     "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
-     "title": "The Boring Company fixing traffic",
-     "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-     "liked": true, "likes": 2345, "replies": 321, "retuits": 432
+        "_id": "1",
+        "topic": "Gods",
+        "userName": "Thor Odinson",
+        "handle": "@thor_odinson",
+        "time": "Feb",
+        "liked": true,
+        "title": "Lorem ipsum dolor sit amet",
+        "tuit": "Mauris tempor, augue ac euismod sollicitudin, libero orci efficitur dolor, non auctor leo sapien eu turpis. Phasellus id pretium sapien, in cursus tellus. Ut tempor feugiat quam, ut ultrices sapien dignissim ut. Mauris tempor, augue ac euismod sollicitudin, libero orci efficitur dolor, non auctor leo sapien eu turpis. Phasellus id pretium sapien, in cursus tellus. Ut tempor feugiat quam, ut ultrices sapien dignissim ut. Mauris tempor, augue ac euismod sollicitudin, libero orci efficitur dolor, non auctor leo sapien eu turpis. Phasellus id pretium sapien, in cursus tellus. Ut tempor feugiat quam, ut ultrices sapien dignissim ut.",
+        "image": "/images/thor.jpg",
+        "replies": 123,
+        "retuits": 432,
+        "likes": 3
     },
     {
-     "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
-     "handle": "@neurolink", "time": "3h", "image": "neurolink.jpg",
-     "title": "Neurolink fixes paralisis and blindness",
-     "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
-     "liked": true, "likes": 3456, "replies": 345, "retuits": 654
-    },
-    ];
+        "_id": "2",
+        "topic": "Mischief",
+        "userName": "Loki Laufeyson",
+        "handle": "loki_laufeyson",
+        "time": "Jul",
+        "liked": true,
+        "title":"Destruction of Asgard",
+        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed condimentum, ante ac mollis bibendum, ante quam porttitor quam, sit amet blandit turpis sapien sed lacus. Sed a odio auctor, consectetur ligula non, fringilla magna. Donec eget velit justo. Cras eu hendrerit felis.",
+        "image": "/images/loki.jpg",
+        "replies": 123,
+        "retuits": 432,
+        "likes": 65
+    }
+]
     
