519973ceef156466b506f1578549457a437b62ce
Sai Dheeraj Reddy Gadwala <drgad24@gmail.com>
2023-03-31 19:15:48 -0400

    cors error fix

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 const app = express()
 app.use(cors())
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 app.use(express.json());
 TuitsController(app);
 HelloController(app)
