00ea4a688de54becfcc75bcb88d42c89b9f6e158
Sai Dheeraj Reddy Gadwala <drgad24@gmail.com>
2023-04-01 23:56:13 -0400

    added dislikes

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -7,9 +7,11 @@ export default [
       "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
       "image": "/images/spacex.webp",
       "liked": true,
+      "disliked":false,
       "replies": 123,
       "retuits": 432,
       "likes": 2345,
+      "dislikes":123,
       "handle": "@spacex",
       "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars"
     },
@@ -20,12 +22,14 @@ export default [
         "handle": "@thor_odinson",
         "time": "Feb",
         "liked": true,
+        "disliked":false,
         "title": "Lorem ipsum dolor sit amet",
         "tuit": "Mauris tempor, augue ac euismod sollicitudin, libero orci efficitur dolor, non auctor leo sapien eu turpis. Phasellus id pretium sapien, in cursus tellus. Ut tempor feugiat quam, ut ultrices sapien dignissim ut. Mauris tempor, augue ac euismod sollicitudin, libero orci efficitur dolor, non auctor leo sapien eu turpis. Phasellus id pretium sapien, in cursus tellus. Ut tempor feugiat quam, ut ultrices sapien dignissim ut. Mauris tempor, augue ac euismod sollicitudin, libero orci efficitur dolor, non auctor leo sapien eu turpis. Phasellus id pretium sapien, in cursus tellus. Ut tempor feugiat quam, ut ultrices sapien dignissim ut.",
         "image": "/images/thor.jpg",
         "replies": 123,
         "retuits": 432,
-        "likes": 3
+        "likes": 3,
+        "dislikes":12,
     },
     {
         "_id": "2",
@@ -34,12 +38,14 @@ export default [
         "handle": "loki_laufeyson",
         "time": "Jul",
         "liked": true,
+        "disliked":true,
         "title":"Destruction of Asgard",
         "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed condimentum, ante ac mollis bibendum, ante quam porttitor quam, sit amet blandit turpis sapien sed lacus. Sed a odio auctor, consectetur ligula non, fringilla magna. Donec eget velit justo. Cras eu hendrerit felis.",
         "image": "/images/loki.jpg",
         "replies": 123,
         "retuits": 432,
-        "likes": 65
+        "likes": 65,
+        "dislikes":37,
     }
 ]
     
