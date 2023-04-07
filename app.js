07cb45bc42c112c4a88d4ab9eece299ba199930f
Sai Dheeraj Reddy Gadwala <drgad24@gmail.com>
2023-04-06 21:18:53 -0400

    A9 Mongo

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import cors from 'cors'
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.MONGO_DB_CONNECTION_WEBDEV || 'mongodb://127.0.0.1:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
 const corsOptions ={
